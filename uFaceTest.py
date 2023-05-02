f93a571132c8734b12af38640e4ddbad953bb163
Juan Diego Monteverde <120123837+juanm0nt@users.noreply.github.com>
2023-05-02 10:25:29 -0400

    Update uFaceTest.py

--- uFaceTest.py
+++ uFaceTest.py
@@ -40,3 +40,25 @@ import cv2
     
 # capture.release()
 # cv2.destroyAllWindows()
+
+# ---OpenCV Python Tutorial 4---
+
+# import numpy as np
+
+# capture = cv2.VideoCapture(0)
+
+# while True:
+#     ret, frame = capture.read()
+#     width = int(capture.get(3))
+#     height = int(capture.get(4))
+    
+#     image = cv2.line(frame, (0, 0), (width, height), (255, 153 ,255), 10)
+#     image = cv2.line(image, (0, height), (width, 0), (255, 153 ,255), 10)
+#     image = cv2.rectangle(image, (800, 500), (300, 300), (128, 128, 128), 5)
+#     image = cv2.circle(image, (400, 400), 50, (0, 0, 255), -1)
+#     font = cv2.FONT_HERSHEY_SIMPLEX
+#     image = cv2.putText(image, "Juan es el mejor", (200 , height - 10), font, 4, (0, 0 ,0), 5, cv2.LINE_AA)
+    
+#     cv2.imshow("Frame", image)
+
+#     if cv2.waitKey(1) == ord("q"):
