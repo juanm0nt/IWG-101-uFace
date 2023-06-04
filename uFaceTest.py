b0e124315effc7704ac2c1550cb4bbc330bd1be7
Juan Diego Monteverde <120123837+juanm0nt@users.noreply.github.com>
2023-06-04 18:03:16 -0400

    Update uFaceTest.py

--- uFaceTest.py
+++ uFaceTest.py
@@ -92,3 +92,26 @@ import cv2
     
 # capture.release()
 # cv2.destroyAllWindows()
+
+# ---OpenCV Python Tutorial 6---
+
+# cap = cv2.VideoCapture(0)
+# cascade_classifier = cv2.CascadeClassifier("/Users/juanmonteverde/juanm0nt.dev/myProjects/uFace/haarcascades/haarcascade_frontalface_default.xml")
+
+# while True:
+    
+#     ret, frame = cap.read()
+#     frame = cv2.cvtColor(frame, 0)
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     detections = cascade_classifier.detectMultiScale(gray, 1.3,5)
+    
+#     if(len(detections) > 0):
+#         (x,y,w,h) = detections[0]
+#         frame = cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+    
+#     cv2.imshow("Frame", frame)
+#     if cv2.waitKey(1) & 0xFF == ord("q"):
+#         break
+    
+# cap.release()
+# cv2.destroyAllWindows()
